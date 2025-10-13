99bb1c41357384d740c443ada2547c5dd0ff3df4
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-10-14 00:16:23 +0530

    mongoodbonnection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
 const app = express();
+
 dotenv.config();
-const PORT = process.env.PORT || 3001;
-app.get('/', (req, res) => {
-  res.send('Hello Bro');
-});
-app.listen(PORT, () => {
-  console.log(`Running on ${PORT}`);
-});
+const PORT = process.env.PORT ; 
+const MONG = process.env.MongoDBURI ; 
+
+mongoose.connect(MONG)
+.then(() => console.log("✅ Connected to MongoDB")).catch((err) => console.error("❌ MongoDB connection error:", err));
+
+app.get('/' , (req,res)=>{
+    res.send("Hello Bro")
+})
+app.listen(PORT , ()=>{
+    console.log(`Runing on ${PORT}`); 
+})
