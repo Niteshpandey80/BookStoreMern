ccc565e605b245f33fd420e79de6550da1fe46e6
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2025-10-15 00:37:16 +0530

    user login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt')
 
 const signup = async (req, res) => {
   try {
@@ -7,7 +8,8 @@ const signup = async (req, res) => {
     if (user) {
       return res.status(400).json({ message: "User Already exists" });
     }
-    const createdUser = new User({ fullname, email, password });
+    const hashPassword = await bcrypt.hash(password , 10)
+    const createdUser = new User({ fullname:fullname, email:email, password:hashPassword });
     await createdUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
@@ -15,6 +17,23 @@ const signup = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
-// ✅ Proper export
-module.exports = { signup };
+const login = async(req,res)=>{
+ try {
+  const {email , password} = req.body ; 
+  const user = await User.findOne({email})
+  const isMatch = await bcrypt.compare(password , user.password)
+  if(!user || !isMatch){
+    return res.status(400).json({message:"Invalid username or password"});
+  }else{
+    res.status(200).json({message:"Login successfully" , user:{
+       _id:user._id, 
+       fullname:user.fullname , 
+       email:user.email
+    }})
+  }
+ } catch (error) {
+  console.log(error);
+  res.status(500).json({message:"Internal Server Error"})
+ }
+}
+module.exports = { signup , login };
